25d0bf7a89cc5d032797de66753235a28cd5cd2a
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-03-20 04:24:30 +0100

    Sidebar menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import "./app.css";
+import SideBar from "./components/sidebar/SideBar";
 import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <SideBar />
+        <div className="others">otherpages</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/SideBar.jsx
@@ -0,0 +1,97 @@
+import {
+  Assessment,
+  Category,
+  Event,
+  Forum,
+  Group,
+  LineStyle,
+  Mail,
+  Message,
+  Receipt,
+  Report,
+  Timeline,
+  TrendingUp,
+} from "@material-ui/icons";
+import "./sideBar.css";
+
+function SideBar() {
+  return (
+    <div className="sideBar">
+      <div className="sideBarWrapper">
+        <div className="sideBarMenu">
+          <h3 className="sideBarTitle">Dashboard</h3>
+          <ul className="sideBarList">
+            <li className="sideBarItem active">
+              <LineStyle className="sideBarIcon" />
+              Home
+            </li>
+            <li className="sideBarItem">
+              <Timeline className="sideBarIcon" />
+              Analytics
+            </li>
+            <li className="sideBarItem">
+              <TrendingUp className="sideBarIcon" />
+              Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sideBarMenu">
+          <h3 className="sideBarTitle">Quick Menu</h3>
+          <ul className="sideBarList">
+            <li className="sideBarItem">
+              <Group className="sideBarIcon" />
+              Users
+            </li>
+            <li className="sideBarItem">
+              <Category className="sideBarIcon" />
+              Products
+            </li>
+            <li className="sideBarItem">
+              <Receipt className="sideBarIcon" />
+              Transactions
+            </li>
+            <li className="sideBarItem">
+              <Assessment className="sideBarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sideBarMenu">
+          <h3 className="sideBarTitle">Notifications</h3>
+          <ul className="sideBarList">
+            <li className="sideBarItem">
+              <Mail className="sideBarIcon" />
+              Mail
+            </li>
+            <li className="sideBarItem">
+              <Forum className="sideBarIcon" />
+              Feedback
+            </li>
+            <li className="sideBarItem">
+              <Message className="sideBarIcon" />
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sideBarMenu">
+          <h3 className="sideBarTitle">Staff</h3>
+          <ul className="sideBarList">
+            <li className="sideBarItem">
+              <Event className="sideBarIcon" />
+              Manage
+            </li>
+            <li className="sideBarItem">
+              <Timeline className="sideBarIcon" />
+              Analytics
+            </li>
+            <li className="sideBarItem">
+              <Report className="sideBarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default SideBar;
